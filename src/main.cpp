f3e51a156ebc1bdff521534b9cf344d4a1d65cc2
Hermance Dornier <hdorn001@hammer.cs.ucr.edu>
2015-01-21 21:06:02 -0800

    Modified main.cpp => cin and $ line, quit possibility

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <string>
+#include <alloca.h>
+#include <cstring>
+#include <sstream>
+#include <algorithm>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 using namespace std;
 
 int main(){
-cout<<"EXXXXTERMINATEEE! --- love, a lonely DALEK"<<endl;
-
+//cout<<"EXXXXTERMINATEEE! --- love, a lonely DALEK"<<endl;
+string cmd;
+while (cmd !="quit"){
+cout<<"$ ";
+getline(cin,cmd);
+cout<<"Nice "<<cmd<<endl;
+}
 return 0;
 }
